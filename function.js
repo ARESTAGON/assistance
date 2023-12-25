ea3741b663dcd17287290dfdcb6b6accd635dd7f
ARESTAGON <94897877+ARESTAGON@users.noreply.github.com>
2023-12-25 16:24:41 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,17 @@
 const select = document.querySelector('select');
 
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function change Language() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  if (!allLang.includes (hash)) {
+    location.href = window.location.pathname + '#' + lang;
+  }
+}
