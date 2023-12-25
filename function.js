6248e3387dc281d9330b7f19af8a5f34a3745339
ARESTAGON <94897877+ARESTAGON@users.noreply.github.com>
2023-12-25 15:33:58 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" charset=utf-8">
+<title>🇺🇦</title>
+<style>
+  body { 
+    overflow: auto;
+    padding-left: auto;
+    background: #000000;
+    color: #FFFFFF;
+  }
+  a:link { 
+    color: #00FF00;
+  }
+  a:visited { 
+    color: #6A5ACD;
+  }
+  .line { 
+    border-bottom: 1px solid #808080;
+  }
+</style>
+</head>
+<body>
+<center><div class="line"></div>
+
+  <marquee scrollamount="3"><h4>🇺🇦 Збройні сили України 🇺🇦</h4></marquee>
+
+<br><div class="line"></div><br>
+
+Потрібна допомога у зборі?
+
+<br>
+
+  Хочеш потрапити до цього списку?
+
+<br><br>
+
+  Звертайся за цими контактами
+
+<br><a href="https://instagram.com/roman.darkov">Instagram</a> | <a href="https://t.me/roman_darkov">Telegram</a><br><br><div class="line"></div><br>
+
+  🇺🇦 Слава Україні 🇺🇦 Героям Слава 🇺🇦
+
+<br><br>
+
+<iframe src="https://www5.cbox.ws/box/?boxid=948460&boxtag=r4XVFc" width="396" height="390" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="396" marginwidth="496" scrolling="auto"></iframe>	
+	
+<br><br><div class="line"></div><br>
+
+<h3>📢 Оголошення №1 📋<br>👑 V.I.P 👑</h3>
+
+<br><br><div class="line"></div><br>
+
+  🚘 Ремонт Автівки 🚘<br>💰 Ціль 100 000 грн 💰
+
+<br><br><img src="https://arestagon.github.io/assistance/broken_car.png" width="396" height="496" alt="Автівка"><br><br>
+
+  💳 Номер Картки 💳<br>5375 4112 1065 2801
+
+<br>🏦 <a href="https://send.monobank.ua/jar/7VGkAFuA6j">Посилання на збір</a> 🏦<br><br><div class="line"></div><br>
+
+  Ім'я військового <a href="https://instagram.com/telis.88">Антон</a>
+
+<br><br><img src="https://arestagon.github.io/assistance/anton_lis.png" width="396" height="496" alt="Антон"><br><br><div class="line"></div><br>
+
+<h3>📢 Оголошення №2 📋<br>вільне</h3>
+
+<br><br><div class="line"></div><br>
+
+  Засновник <a href="https://instagram.com/roman.darkov">Roman Darkov</a>
+
+<br>
+
+Допомога Армії України © 2023-2024
+
+<br><br><div class="line"></div>
+  
+</center>
+</body>
+</html>
